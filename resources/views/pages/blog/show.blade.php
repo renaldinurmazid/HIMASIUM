14ea166b56822221f6b1cd8dc9bab263de57168b
renaldinurmazid <renaldinurmazid@gmail.com>
2026-04-17 09:13:57 +0700

    feat: integrate Disqus comment section into blog post view

--- resources/views/pages/blog/show.blade.php
+++ resources/views/pages/blog/show.blade.php
@@ -47,6 +47,8 @@
             <div class="space-y-4">
                 {!! str($post->content)->markdown()->sanitizeHtml() !!}
             </div>
+            <br><br>
+            <div id="disqus_thread"></div>
 
             <!-- Footer Author -->
             <div class="mt-24 pt-12 border-t border-slate-100 flex flex-col md:flex-row items-center border-b pb-12">
@@ -61,4 +63,22 @@
             </div>
         </div>
     </article>
+    <script>
+        /**
+        *  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
+        *  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables    */
+        /*
+        var disqus_config = function () {
+        this.page.url = PAGE_URL;  // Replace PAGE_URL with your page's canonical URL variable
+        this.page.identifier = PAGE_IDENTIFIER; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+        };
+        */
+        (function() { // DON'T EDIT BELOW THIS LINE
+        var d = document, s = d.createElement('script');
+        s.src = 'https://himasium.disqus.com/embed.js';
+        s.setAttribute('data-timestamp', +new Date());
+        (d.head || d.body).appendChild(s);
+        })();
+    </script>
+    <noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
 @endsection
